806a5ec8176d7b39ae004eb542ae170fa04723b8
Charles Calapini <7816568+calapsss@users.noreply.github.com>
2022-04-20 05:22:51 -0600

    Update
    patch

--- frontend/src/shared/components/Navigation/MainNavigation.js
+++ frontend/src/shared/components/Navigation/MainNavigation.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, {useState}  from 'react';
 import { Link } from 'react-router-dom';
 
 import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
+
+import SideDrawer from './SideDrawer';
 import './MainNavigation.css';
+import Backdrop from '../UIElements/Backdrop';
 
 const MainNavigation = props => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDrawer = () =>{
+    setDrawerIsOpen(true);
+  }
+
+  const closeDrawer = () =>{
+    setDrawerIsOpen(false);
+  }
+
   return (
+    <React.Fragment>
+    { drawerIsOpen && <Backdrop onClick={closeDrawer} />
+    }
+
+
+    {drawerIsOpen && (
+    <SideDrawer>
+      <nav className = "main-navigation__drawer-nav">
+        <NavLinks />
+      </nav>
+    </SideDrawer>)}
     <MainHeader>
-      <button className="main-navigation__menu-btn">
+      <button className="main-navigation__menu-btn" onClick={openDrawer}>
         <span />
         <span />
         <span />
@@ -16,10 +40,12 @@ const MainNavigation = props => {
       <h1 className="main-navigation__title">
         <Link to="/">YourPlaces</Link>
       </h1>
-      <nav>
+      <nav className="main-navigation_header-nav">
         <NavLinks />
       </nav>
     </MainHeader>
+
+    </React.Fragment>
   );
 };
 
---
+++ frontend/src/shared/components/UIElements/Backdrop.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import './Backdrop.css';
+
+
+const Backdrop = props => {
+    return ReactDOM.createPortal(
+        <div className='backdrop' onClick={props.onClick}></div>,
+        document.getElementById('backdrop-hook')
+
+    );
+
+
+
+
+};
+
+
+
+export default Backdrop
